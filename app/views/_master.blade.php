648f985d05146e6b143debd216e578d21c54919c
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-13 15:20:11 -0400

    added all unadded files

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -6,7 +6,7 @@
 	<title>@yield('title','Developers Best Friend')</title>
 
 	
-	<link rel="stylesheet" href="style.css" type="text/css">
+	<link rel="stylesheet" href="views/style.css" type="text/css">
 
 	@yield('head')
 
--- app/views/user_generator.blade.php
+++ app/views/user_generator.blade.php
@@ -16,20 +16,20 @@
 	{{ Form::label('number_of_users', 'Number of users:', array('id' => 'user_generator')) }}
 	{{ Form::text('number_of_users', '1') }}
 	<br>
+	{{ Form::label('add_birthday', 'Include birthday') }}
+	{{ Form::checkbox('add_birthday', '1', true) }}
+	<br>
 	{{ Form::label('add_address', 'Include address') }}
 	{{ Form::checkbox('add_address', '1', true) }}
 	<br>
 	{{ Form::label('add_profile', 'Include profile') }}
 	{{ Form::checkbox('add_profile', '1', true) }}
 	<br>
-	<!--
-	{{ Form::label('add_photo', 'Include photo') }}
-	{{ Form::checkbox('add_photo', '1', true) }}
-	-->
-	<br>
+	
 	{{ Form::submit('Generate!') }}
 	
 	{{ Form::close() }}
+	<br>
 </dev>
 
 <?php 
@@ -42,12 +42,12 @@ if(isset($_POST["number_of_users"])) {
 		
 		echo $faker->name. "<br />";
 
-		//if ($_POST["add_photo"] == true) {
-		//	echo $faker->image($dir = '/tmp', $width = 640, $height = 480);
-		//}
+		if ($_POST["add_birthday"] == true) {
+			echo $faker->dateTimeThisCentury->format('Y-m-d'). "<br />";
+		}
 
 		if ($_POST["add_address"] == true) {
-			echo $faker->address;
+			echo $faker->address. "<br />";
 		}
 		if ($_POST["add_profile"] == true) {
 			echo $faker->text;

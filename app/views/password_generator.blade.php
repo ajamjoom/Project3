91ddf48b880521510c9d9171956a3caff0bb1908
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2014-07-15 13:28:12 -0400

    chnaged the textbox field to select box to put a limit and make sure that users can only input numbers from 1-10(in the xkcd password generator)

--- app/views/password_generator.blade.php
+++ app/views/password_generator.blade.php
@@ -18,7 +18,20 @@
 	{{ Form::open(array('url' => '/password_generator', 'method' => 'POST')) }}
 	
 	{{ Form::label('number_of_words', 'Number of words:', array('id' => 'password_generator')) }}
-	{{ Form::text('number_of_words', '1') }}
+	{{Form::select('number_of_words', array(
+		
+		'1' => '1',
+		'2' => '2',
+		'3' => '3',
+		'4' => '4',
+		'5' => '5',
+		'6' => '6',
+		'7' => '7',
+		'8' => '8',
+		'9' => '9',
+		
+		'10' => '10'
+), '1' ) }}
 	<br>
 	{{ Form::label('add_number', 'Add a number') }}
 	{{ Form::checkbox('add_number', '1', false) }}
